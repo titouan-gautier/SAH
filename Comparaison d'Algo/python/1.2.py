f68a66317505cf05b6f9c8b2d573c59203e99de7
JulesRosaclot <julesrosaclot@gmail.com>
2021-11-18 15:17:06 +0100

    tamere

--- Comparaison d'Algo/python/1.2.py
+++ Comparaison d'Algo/python/1.2.py
@@ -1,4 +1,14 @@
 
 
 def extented_gcd(a, b) :
-    
+    if a == 0 :
+        return (b, 0, 1)
+    d, u, v = extented_gcd(b%a, b)
+
+u1 = v - (b//a) * u
+v1 = u
+
+a = int(input("Saisir le premier terme: "))
+b = int(input("Saisir le second terme: "))
+d, u1, v1 = extented_gcd(a, b)
+print(d, u1, v1)
