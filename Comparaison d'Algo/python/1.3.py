58e5694ae224bbf4d2dbb7cb114c3173c520749c
GAUTIER <titgautier@gmail.com>
2021-12-03 15:45:23 +0100

    03/12/2021_Titouan2

--- Comparaison d'Algo/python/1.3.py
+++ Comparaison d'Algo/python/1.3.py
@@ -29,19 +29,30 @@ def extended_gcd(a,b):
     return a,u0,u1
 
 
-
+def extended_gcd(a,b):
+    if a < b : 
+        a,b = b,a
+    u0, u1 = 1, 0
+    v0, v1 = 0, 1
+    while b != 0 :
+        q = int(a/b)
+        b, a = a%b, b
+        u0, u1, v0, v1 = v0, v1, u0-q*v0, u1-q*v1
+    return a,u0,u1
 
 def key_creation() :
     p = random.choice(list_prime(1000))
     q = random.choice(list_prime(1000))
     n = p*q
     fiN = (p-1)*(q-1)
-    e = 0
+    e = 2
     a = 0
     while a != 1 :
         a,u0,u1 = extended_gcd(e, fiN)
-        e += 1
+        if a != 1 :
+            e = e + 1
         pub = (n,e)
-    return n,pub
+    
+    return p,q,n,pub
 
 print(key_creation())
